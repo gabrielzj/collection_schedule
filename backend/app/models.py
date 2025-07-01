6d2db59faf586b25d4b1cd90162fc3e44a1e3067
gabrielzj <gabrielzuinj@gmail.com>
2025-06-30 23:57:09 -0300

    init config app, serializers and models

---
+++ backend/app/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=100, null=False, blank=False)
+    password = models.CharField(max_length=128, null=False, blank=False)
+    email = models.EmailField(unique=True, null=True, blank=True)
+    address = models.CharField(max_length=255, null=False, blank=False)
+    phone_number = models.CharField(max_length=15, null=False, blank=False)
+    
+    class Meta:
+        verbose_name = 'Usuário'
+        verbose_name_plural = 'Usuários'
+        ordering = ['username']
+        
+    def __str__(self):
+        return self.username
+    
+class CollectionCall(models.Model):
+    
+    CHOICES_URGENCY = [
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    ]
+    
+    STATUS = [
+        ('pending', 'Pending'),
+        ('completed', 'Completed'),
+        ('failed', 'Failed'),
+    ]
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='collection_calls', verbose_name='Usuário Responsável') 
+    call_date = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    amount_to_collected = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+    urgency = models.CharField(max_length=50, choices=CHOICES_URGENCY, default='low')
+    status = models.CharField(max_length=50, choices=STATUS, default='pending')
+    best_time_for_collection = models.DateTimeField(null=True, blank=True)
+    
+    class Meta:
+        verbose_name = 'Chamado de Coleta'
+        verbose_name_plural = 'Chamados de Coleta'
+        ordering = ['-call_date']
+
+    def __str__(self):
+        return f"Chamado de coleta criado por {self.user.username} em {self.call_date} - Status: {self.status}"
---
+++ backend/app/serializers.py
@@ -0,0 +1,49 @@
+from .models import User, CollectionCall
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+        
+    def validate_password(self, value):
+        if len(value.get('password')) < 8:
+            raise serializers.ValidationError(
+                {"password":"A senha deve conter pelo menos 8 caracteres."}
+            )
+        return value
+
+    def validate_phone_number(self, value):
+        if not value.isdigit() or len(value) < 10:
+            raise serializers.ValidationError(
+                {"phone_number":"O número de telefone deve conter apenas números e ter pelo menos 10 dígitos."}
+            )
+        return value
+    
+class CollectionCallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CollectionCall
+        fields = '__all__'
+        
+    def validade_user(self, value):
+        if not User.objects.filter(id=value.id).exists():
+            raise serializers.ValidationError(
+                {"user": "Usuário vinculado ao chamado não encontrado."}
+            )
+        return value
+    
+    def validade_amount_to_collected(self, value):
+        if value <= 0:
+            raise serializers.ValidationError(
+                {"amount_to_collected": "O valor a ser coletado deve ser maior que zero."}
+            )
+        elif value.isstring():
+            raise serializers.ValidationError(
+                {"amount_to_collected": "O valor a ser coletado deve ser um número."}
+            )
+        elif value > 10000:
+            raise serializers.ValidationError(
+                {"amount_to_collected": "Não é posssível criar um chamado de coleta com essa quantia."}
+            )
+        return value
+
