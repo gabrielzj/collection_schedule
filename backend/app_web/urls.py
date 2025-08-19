f33c6bca9d7dc953151cc40d61b5fa5bdd76e228
gabrielzj <gabrielzuinj@gmail.com>
2025-08-19 18:16:23 -0300

    chore: app_web urls path

--- backend/app_web/urls.py
+++ backend/app_web/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('auth/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', CustomTokenObtainPairView.as_view(), name='token_refresh'),
     path('auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('register/', views.register_user, name='user_register'),
-    path('call/list/', views.list_calls, name='list_calls'),
+    path('users/', views.register_user, name='user_register'),
+    path('calls/', views.list_calls, name='list_calls'),
 ]
