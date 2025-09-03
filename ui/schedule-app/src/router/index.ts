dfa6c69b0fc60170cfa13c9b8ede95bed65b9559
gabrielzj <gabrielzuinj@gmail.com>
2025-09-03 00:37:02 -0300

    update: page routes

--- ui/schedule-app/src/router/index.ts
+++ ui/schedule-app/src/router/index.ts
@@ -3,40 +3,44 @@ import { RouteRecordRaw } from "vue-router";
 import TabsPage from "../views/TabsPage.vue";
 
 const routes: Array<RouteRecordRaw> = [
-    {
-        path: "/",
-        redirect: "/home",
-    },
-    {
-        path: "",
-        redirect: "/home",
-    },
-    {
-        path: "/login",
-        component:  () => import("@/views/login-page.vue"),
-    },
-    {
-        path: "/home",
-        component: TabsPage,
-        children: [
-          {
-            path: "waste-info",
-            component: () => import("@/views/Tab1Page.vue"),
-          },
-          {
-            path: "call",
-            component: () => import("@/views/Tab2Page.vue"),
-          },
-          {
-            path: "login",
-            component: () => import("@/views/login-page.vue"),
-          },
-          {
-            path: "tab4",
-            component: () => import("@/views/Tab3Page.vue"),
-          },
-        ],
-    },
+  {
+    path: "/",
+    redirect: "/login",
+  },
+  {
+    path: "",
+    redirect: "/login",
+  },
+  {
+    path: "/login",
+    component: () => import("@/views/login-page.vue"),
+  },
+  {
+    path: "/register",
+    component: () => import("@/views/register.vue"),
+  },
+  {
+    path: "/home",
+    component: TabsPage,
+    children: [
+      {
+        path: "waste-info",
+        component: () => import("@/views/Tab1Page.vue"),
+      },
+      {
+        path: "call",
+        component: () => import("@/views/Tab2Page.vue"),
+      },
+      {
+        path: "login",
+        component: () => import("@/views/login-page.vue"),
+      },
+      {
+        path: "tab4",
+        component: () => import("@/views/Tab3Page.vue"),
+      },
+    ],
+  },
 ];
 
 const router = createRouter({
