8a458008bd9038156a041180f22d1716342b3274
gabrielzj <gabrielzuinj@gmail.com>
2025-09-03 00:36:34 -0300

    update: SimpleJWT config

--- backend/schedule_app/settings.py
+++ backend/schedule_app/settings.py
@@ -38,6 +38,10 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'AUTH_HEADER_TYPES': ('Bearer',),
     'USERNAME_FIELD': 'email',
 }
 
