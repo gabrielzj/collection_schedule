579176b91fb199289eb21393ed8a1c4bad45f58e
gabrielzj <gabrielzuinj@gmail.com>
2025-09-15 22:38:49 -0300

    added logout component

---
+++ ui/schedule-app/src/views/logout.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Tab 3</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 3</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <ExploreContainer name="Tab 3 page" />
+    </ion-content>
+  </ion-page> -->
+</template>
+
+<script setup lang="ts">
+import router from "@/router";
+import { onMounted } from "vue";
+
+// import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+// import ExploreContainer from '@/components/ExploreContainer.vue';
+
+onMounted(() => {
+  localStorage.removeItem("access_token");
+  localStorage.removeItem("refresh_token");
+  localStorage.removeItem("user_id");
+  router.replace("/login");
+});
+</script>
