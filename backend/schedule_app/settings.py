3ea11c7b8c1cfb66836a0899755d8a8056d33cbe
gabrielzj <gabrielzuinj@gmail.com>
2025-09-28 01:43:00 -0300

    add: local timezone

--- backend/schedule_app/settings.py
+++ backend/schedule_app/settings.py
@@ -154,9 +154,9 @@ LOGGING = {
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
