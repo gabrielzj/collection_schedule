c03d092b873cfff8ba9ebdf73b4ed54fa05ade5f
gabrielzj <gabrielzuinj@gmail.com>
2025-10-24 17:00:31 -0300

    update: labels style and more

--- ui/admin-web/src/components/CollectionCard.vue
+++ ui/admin-web/src/components/CollectionCard.vue
@@ -9,18 +9,18 @@
     <p class="line">
         <p v-if="call.user.first_name && call.user.last_name"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
           <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
-        </svg><strong>Usuário:</strong>
+        </svg><span class="label"><strong>Usuário:</strong></span>
         {{ call.user?.first_name }} {{ call.user?.last_name }}
         </p>
         <p v-else><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
           <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
-        </svg><strong>Usuário:</strong> <span>Sem Nome</span>
+        </svg><span class="label"><strong>Usuário:</strong></span> <span>Sem Nome</span>
         </p>
         <div class="email-container"> 
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
             <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z"/>
           </svg>
-          <strong>E-mail: </strong>
+          <span class="label"><strong>E-mail: </strong></span> 
           <span v-if="call.user?.email" class="email"> {{ call.user.email }}</span>
         </div>
     </p>
@@ -29,13 +29,13 @@
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
       </svg>
-      <strong>Telefone:</strong> {{ call.user.phone_number || '-' }}
+      <span class="label"><strong>Telefone:</strong></span> {{ call.user.phone_number || '-' }}
     </p>
     <p class="line">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
         <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
       </svg>
-      <strong>Endereço de Residência:</strong> <br> {{ call.user.address || '-' }}
+      <span class="label"><strong>Endereço de Residência:</strong></span> <br> {{ call.user.address || '-' }}
     </p>
     <!-- <p class="line">
       <strong>Melhor Data/Horário de Coleta:</strong>
@@ -207,7 +207,12 @@ function formatDate(value?: string | null) {
 }
 
 svg {
+  color: #032b81;
   margin-right: 5px;
+}
+
+p .label{
+  color: #2f7dd1;
 }
 .qtd {
     background: #8eaff5;
