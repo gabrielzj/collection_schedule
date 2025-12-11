8ff5ce9169fd4830076275ea544af045b7ff27ff
gabrielzj <gabrielzuinj@gmail.com>
2025-12-11 03:04:18 -0300

    schedule-app/add: config to allow HTTP request to android emulador

--- ui/schedule-app/capacitor.config.ts
+++ ui/schedule-app/capacitor.config.ts
@@ -1,9 +1,13 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'schedule-app',
-  webDir: 'dist'
+  appId: "io.ionic.starter",
+  appName: "schedule-app",
+  webDir: "dist",
+  server: {
+    androidScheme: "http",
+    cleartext: true,
+  },
 };
 
 export default config;
